77dc181be9e227811b5aaaf32ef2fb1345003517
minsk601 <minsk601@naver.com>
2025-07-21 23:47:22 +0900

    Changing the design

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-    const sidebar = document.querySelector('.sidebar');
-    const toggleBtn = document.querySelector('.sidebar-toggle');
-
-    toggleBtn.addEventListener('click', function() {
-        sidebar.classList.toggle('is-hidden');
-    });
-});
